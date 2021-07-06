bc8b45657b3c83d16b97667db9143b6f01249766
Zenghui Wang <952568507@qq.com>
2021-07-07 01:26:44 +0800

    0707 flink source

---
+++ src/main/java/com/jw9j/source/SourceTest3_Kafka.java
@@ -0,0 +1,18 @@
+package com.jw9j.source;
+
+import org.apache.flink.streaming.api.datastream.DataStream;
+import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+
+/**
+ * 从kafka读取数据
+ * @Author jw9j
+ * @create 2021/7/7 1:20
+ */
+public class SourceTest3_Kafka {
+
+    public static void main(String[] args) {
+        StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
+//
+//        DataStream<String> dataStream = env.addSource()
+    }
+}
