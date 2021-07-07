18b56589725cfa9597bcf3ae65a036067a1a8b6f
Zenghui Wang <952568507@qq.com>
2021-07-08 01:25:30 +0800

    0708 flink kafka transform

--- src/main/java/com/jw9j/source/SourceTest3_Kafka.java
+++ src/main/java/com/jw9j/source/SourceTest3_Kafka.java
@@ -1,7 +1,11 @@
 package com.jw9j.source;
 
+import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
+
+import java.util.Properties;
 
 /**
  * 从kafka读取数据
@@ -10,9 +14,18 @@ import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
  */
 public class SourceTest3_Kafka {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
-//
+        Properties properties = new Properties();
+        properties.setProperty("bootstrap.servers","localhost:29092");
+        properties.setProperty("group.id","consumer-group");
+        properties.setProperty("auto.offset.reset","latest");
+
+        DataStream<String> dataStream = env.addSource(new FlinkKafkaConsumer<String>("sensor",new SimpleStringSchema(),properties));
 //        DataStream<String> dataStream = env.addSource()
+
+        dataStream.print();
+
+        env.execute();
     }
 }
